26e71b3787be25c2b7f1a5eaf44846416947eb9d
ccalvocm <52244870+ccalvocm@users.noreply.github.com>
2021-10-04 16:41:50 -0300

    Update instalar_mamba.bat

--- instalar_mamba.bat
+++ instalar_mamba.bat
@@ -1,6 +1,6 @@
 powershell.exe -command "& Invoke-Webrequest -URI https://micromamba.snakepit.net/api/micromamba/win-64/latest -OutFile micromamba.tar.bz2"
 C:\PROGRA~1\7-Zip\7z.exe x micromamba.tar.bz2 -aoa
-C:\PROGRA~1\7-Zip\7z.exe x micromamba.tar -ttar -aoa -r .\Library\bin\micromamba.exe
+C:\PROGRA~1\7-Zip\7z.exe x micromamba.tar -ttar -aoa -r Library\bin\micromamba.exe
 $Env:MAMBA_ROOT_PREFIX=(Join-Path -Path $HOME -ChildPath micromamba)
 $Env:MAMBA_EXE=(Join-Path -Path (Get-Location) -ChildPath micromamba.exe)
-.\Library\bin\micromamba.exe create -f ./test/env_win.yaml -y
+.\Library\bin\micromamba.exe create -f pySRM.yaml -y
