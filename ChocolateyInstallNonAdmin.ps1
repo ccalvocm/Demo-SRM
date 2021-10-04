3d83cf401468cbe4a7eff15fcaaf14bca775f12b
ccalvocm <52244870+ccalvocm@users.noreply.github.com>
2021-10-04 19:41:07 -0300

    instalar miniconda

--- ChocolateyInstallNonAdmin.ps1
+++ ChocolateyInstallNonAdmin.ps1
@@ -6,15 +6,8 @@ $env:ChocolateyInstall="$InstallDir"
 # If your PowerShell Execution policy is restrictive, you may
 # not be able to get around that. Try setting your session to
 # Bypass.
-powershell.exe -command "Set-ExecutionPolicy Bypass -Scope Process -Force;"
+Set-ExecutionPolicy Bypass -Scope Process -Force;
 
 # All install options - offline, proxy, etc at
 # https://chocolatey.org/install
 iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-
-#instalar git
-choco install git.commandline -y
-
-#intalar miniconda
-choco install miniconda3 --params="'/AddToPath:1 /InstallationType:JustMe'" -y
-%UserProfile%\Miniconda3\_conda.exe env create -f .\pySRM_win.yaml
