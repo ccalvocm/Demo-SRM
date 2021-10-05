de759d74bec2710bf25cf29f197929b7d5aa300d
ccalvocm <52244870+ccalvocm@users.noreply.github.com>
2021-10-04 22:09:11 -0300

    Update Bajar_SRM_mamba.bat

--- Bajar_SRM_mamba.bat
+++ Bajar_SRM_mamba.bat
@@ -1,4 +1,4 @@
 git clone https://github.com/ccalvocm/Demo-SRM.git &
 call conda create -n pySRM python=3.8 -y &
-call conda install mamba -n base -c conda-forge -y & 
+call conda install mamba -n base -c conda-forge -y &
 call mamba env update -n pySRM --file .\Demo-SRM\pySRM_win.yaml
