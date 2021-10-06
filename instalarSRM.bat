18acecc4d88b14b60305a1b2466c43230197c0b3
ccalvocm <52244870+ccalvocm@users.noreply.github.com>
2021-10-06 13:29:20 -0300

    install files

--- instalarSRM.bat
+++ instalarSRM.bat
@@ -1,3 +1,3 @@
 Powershell.exe -Command "Set-ExecutionPolicy Bypass -Scope Process -Force;"
 PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& './ChocolateyInstallNonAdmin.ps1'"
-PowerShell -NoProfile -ExecutionPolicy Bypass -file ./gitCondaInstall.ps1
+start cmd /c PowerShell -NoProfile -ExecutionPolicy Bypass -file ./gitCondaInstall.ps1
