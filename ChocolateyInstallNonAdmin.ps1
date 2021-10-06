c07e526cc71398b092edad7ba71dd88106af51ec
ccalvocm <52244870+ccalvocm@users.noreply.github.com>
2021-10-06 12:49:51 -0300

    install files

--- ChocolateyInstallNonAdmin.ps1
+++ ChocolateyInstallNonAdmin.ps1
@@ -21,27 +21,3 @@ $intButton = $sh.Popup("git instalado correctamente",0,"Instalador SRM",0+64)
 #intalar miniconda
 choco install miniconda3 --params="'/AddToPath:1 /InstallationType:JustMe'" -y
 $intButton = $sh.Popup("miniconda instalado correctamente",0,"Instalador SRM",0+64)
-
-#recargar entorno
-refreshenv
-
-#chequear carpeta del modelo SRM
-if (Test-Path -Path Demo-SRM\) {
-    cd Demo-SRM\
-    git pull
-    cd ..
-    $intButton = $sh.Popup("SRM actualizado correctamente",0,"Instalador SRM",0+64)
-} else {
-    git clone https://github.com/ccalvocm/Demo-SRM.git
-    $intButton = $sh.Popup("SRM descargado correctamente",0,"Instalador SRM",0+64)
-}
-
-#crear entorno virtual
-conda create -n pySRM python=3.8 -y
-
-#instalar mamba
-conda install mamba -n base -c conda-forge -y
-
-#actualizar entorno virtual
-mamba env update -n pySRM --file .\Demo-SRM\pySRM_win.yaml
-$intButton = $sh.Popup("SRM configurado correctamente",0,"Instalador SRM",0+64)
