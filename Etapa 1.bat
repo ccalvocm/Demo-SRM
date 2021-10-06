6e5fed2f49f085cfd2762ffee82ba327f74ced42
ccalvocm <52244870+ccalvocm@users.noreply.github.com>
2021-10-06 18:23:37 -0300

    install update

--- Etapa 1.bat
+++ Etapa 1.bat
@@ -1,3 +1,4 @@
 Powershell.exe -Command "Set-ExecutionPolicy Bypass -Scope Process -Force;"
+Powershell.exe -NoProfile -ExecutionPolicy unrestricted -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 Powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "& './ChocolateyInstallNonAdmin.ps1'"
 mshta "about:<script>alert('Git instalado existosamente, ejecutar Etapa2.bat');close()</script>"
