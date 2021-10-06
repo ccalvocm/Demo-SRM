c51c7982868660924f1832604648cadb78c1f09b
ccalvocm <52244870+ccalvocm@users.noreply.github.com>
2021-10-06 11:23:28 -0300

    insta

--- instalarMiniconda.bat
+++ instalarMiniconda.bat
@@ -2,9 +2,12 @@ Powershell.exe -Command "Set-ExecutionPolicy Bypass -Scope Process -Force;"
 PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& './ChocolateyInstallNonAdmin.ps1'" & if exist Demo-SRM\ (CD Demo-SRM\
 call git pull
 CD ..
+call mshta "about:<script>alert('Chocolatey & miniconda instalados');close()</script>"
 ) else (
 call git clone https://github.com/ccalvocm/Demo-SRM.git
+call mshta "about:<script>alert('SRM descargado');close()</script>"
 )
 call conda create -n pySRM python=3.8 -y &
 call conda install mamba -n base -c conda-forge -y &
-call mamba env update -n pySRM --file .\Demo-SRM\pySRM_win.yaml
+call mamba env update -n pySRM --file .\Demo-SRM\pySRM_win.yaml &
+call mshta "about:<script>alert('Anaconda configurado');close()</script>"
