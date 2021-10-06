5c35b9033ca7035824eed7fa38932a9b0be6f14f
ccalvocm <52244870+ccalvocm@users.noreply.github.com>
2021-10-06 11:07:39 -0300

    call instaler

--- instalarMiniconda.bat
+++ instalarMiniconda.bat
@@ -1,6 +1,6 @@
 Powershell.exe -Command "Set-ExecutionPolicy Bypass -Scope Process -Force;"
 PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& './ChocolateyInstallNonAdmin.ps1'"
-if exist Demo-SRM\ (CD Demo-SRM
+if exist Demo-SRM\ (CD Demo-SRM\
 call git pull
 CD ..
 ) else (
