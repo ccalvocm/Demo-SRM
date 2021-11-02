8725fba5a1f5b6f43cc395eb6f1efbc819ceb26e
ccalvocm <52244870+ccalvocm@users.noreply.github.com>
2021-11-02 18:22:31 -0300

    pre unc RMEEM

--- 01_Maipo/02_RMEEM/SRM/Inputs/pyCSRM.py
+++ 01_Maipo/02_RMEEM/SRM/Inputs/pyCSRM.py
@@ -192,7 +192,7 @@ def DEVELOP_SRM(root, Basin, alpha, Tcrit, plots):
     master = pd.read_csv(root+r'/Master.csv', index_col = 0, parse_dates = True)
     
     # última fecha con datos 
-    lastDate = pd.to_datetime('2021-05-29')
+    lastDate = pd.to_datetime('2022-03-31')
         
     # último dia de la simulacion anterior
     FirstDay = 0
@@ -486,20 +486,20 @@ def DEVELOP_SRM(root, Basin, alpha, Tcrit, plots):
         swe_sim = np.sum(snowAcc*A, axis = 1) / Atot
         swe_sim = pd.DataFrame(swe_sim, index = dates, columns = ['SWE simulado']) # SWE en m
 
-        # SWe observado
-        swe_obs = pd.read_csv(r'C:\Users\ccalvo\OneDrive - ciren.cl\Of hidrica\AOHIA_ZC\Etapa 3\Scripts\outputs\GEE_downloads\Maipo\MEEM\Maipo_MEEM_bandas_ERA5hourly_SWE_2000_2021_simplify_0_05.csv', index_col = 0, parse_dates = True)
-        swe_obs_day = swe_obs.resample('D').mean()
-        swe_obs_day.iloc[:,-1] = swe_obs_day.iloc[:,-2]
-        swe_obs_day = swe_obs_day.loc[dates]
-        swe_obs_day = np.sum(swe_obs_day.values*A, axis = 1) / Atot
-        swe_obs_day = pd.DataFrame(swe_obs_day, index = dates, columns = ['SWE observado']) # SWE en m
+        # # SWe observado
+        # swe_obs = pd.read_csv(r'C:\Users\ccalvo\OneDrive - ciren.cl\Of hidrica\AOHIA_ZC\Etapa 3\Scripts\outputs\GEE_downloads\Maipo\MEEM\Maipo_MEEM_bandas_ERA5hourly_SWE_2000_2021_simplify_0_05.csv', index_col = 0, parse_dates = True)
+        # swe_obs_day = swe_obs.resample('D').mean()
+        # swe_obs_day.iloc[:,-1] = swe_obs_day.iloc[:,-2]
+        # swe_obs_day = swe_obs_day.loc[dates]
+        # swe_obs_day = np.sum(swe_obs_day.values*A, axis = 1) / Atot
+        # swe_obs_day = pd.DataFrame(swe_obs_day, index = dates, columns = ['SWE observado']) # SWE en m
         
-        # plots
-        fig, ax = plt.subplots(1)
-        swe_sim.plot(ax = ax)   
-        swe_obs_day.plot(ax = ax)        
-        plt.ylabel('Equivalente en agua de nieve (m)')
-        plt.grid()
+        # # plots
+        # fig, ax = plt.subplots(1)
+        # swe_sim.plot(ax = ax)   
+        # swe_obs_day.plot(ax = ax)        
+        # plt.ylabel('Equivalente en agua de nieve (m)')
+        # plt.grid()
        
 #%%    
     #==========================================================================
@@ -517,5 +517,5 @@ def DEVELOP_SRM(root, Basin, alpha, Tcrit, plots):
 if __name__ == '__main__':
     root = '.'
     Basin = 'Maipo_en_el_Manzano'
-    DEVELOP_SRM(root, Basin, alpha = 0.959, Tcrit = 1, plots = True)
+    DEVELOP_SRM(root, Basin, alpha = 0.959, Tcrit = 1, plots = False)
     
--- uncertainty_analysis.py
+++ uncertainty_analysis.py
@@ -8,11 +8,9 @@ Created on Mon Oct 25 12:44:25 2021
 import pandas as pd
 import os
 from subprocess import Popen
-import time
 from os.path import isfile, join
 
 def list_out(directory):
-    # files = [f for f in listdir(directory) if (isfile(join(directory, f))) & (f.endswith('.hdf') | f.endswith('.tif'))]
     files = [f for f in os.listdir(directory) if (isfile(join(directory, f))) & (f.endswith('.out'))]
     return files
 
@@ -22,10 +20,21 @@ def main():
     # rutas
     path_unc_in = r'C:\Users\ccalvo\Documents\GitHub\Demo SRM\Demo-SRM\01_Maipo\01_RMELA\SRM\Inputs\unc_analysis.in'
     path_master = r'C:\Users\ccalvo\Documents\GitHub\Demo SRM\Demo-SRM\01_Maipo\01_RMELA\SRM\Inputs\Master.pst'
+    
+    path_unc_in = r'C:\Users\ccalvo\Documents\GitHub\Demo SRM\Demo-SRM\01_Maipo\02_RMEEM\SRM\Inputs\unc_analysis.in'
+    path_master = r'C:\Users\ccalvo\Documents\GitHub\Demo SRM\Demo-SRM\01_Maipo\02_RMEEM\SRM\Inputs\Master.pst'    
 
 
     unc_in = pd.read_csv(path_unc_in)
+    
+    # Río Mapocho en Los Almendros
     master = pd.read_csv(path_master, skiprows = 7618, nrows=365, sep = '\t', header = None, index_col = 0)
+    # Río Maipo en el Manzano
+    master = pd.read_csv(path_master, skiprows = 7356, nrows=365, sep = ' ', header = None, index_col = 1)
+    
+    # ruta de archivos de simulacion
+    # folder = r'C:\Users\ccalvo\Documents\GitHub\Demo SRM\Demo-SRM\01_Maipo\01_RMELA\SRM\Inputs'
+    folder = r'C:\Users\ccalvo\Documents\GitHub\Demo SRM\Demo-SRM\01_Maipo\02_RMEEM\SRM\Inputs'
     
     # iterar sobre caudales
     for caudal in master.index:
@@ -40,12 +49,12 @@ def main():
         unc_in.to_csv(path_unc_in, index = None)
     
         # ejecutar el análisis de incertidumbre
-        ruta_bat = r"C:\Users\ccalvo\Documents\GitHub\Demo SRM\Demo-SRM\01_Maipo\01_RMELA\SRM\Inputs"
+        # ruta_bat = r"C:\Users\ccalvo\Documents\GitHub\Demo SRM\Demo-SRM\01_Maipo\01_RMELA\SRM\Inputs"
+        ruta_bat = folder
         p = Popen("run_uncert.bat", cwd=ruta_bat)
         p.communicate()
 
     # juntar las desviaciones estandar
-    folder = r'C:\Users\ccalvo\Documents\GitHub\Demo SRM\Demo-SRM\01_Maipo\01_RMELA\SRM\Inputs'
     listout = list_out(folder)
     listout.sort()
     
@@ -55,7 +64,8 @@ def main():
     # leer las desviaciones estandar
     for file in listout:
         file_read = pd.read_csv(os.path.join(folder,file))
-        std_dev = float(file_read.loc[173].values[0].split('=')[-1].split()[0])
+        # std_dev = float(file_read.loc[173].values[0].split('=')[-1].split()[0])
+        std_dev = float(file_read.loc[196].values[0].split('=')[-1].split()[0])
         lista_std_dev.append(std_dev)
     
     df_std_dev = pd.DataFrame(lista_std_dev, columns = ['Desviacion estandar (l/s)'])
