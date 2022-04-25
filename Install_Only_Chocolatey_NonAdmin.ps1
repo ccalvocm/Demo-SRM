1c0176a7d178bd620681cbf93579b2888a65c32f
felipearrospide <62312466+felipearrospide@users.noreply.github.com>
2022-04-25 16:30:37 -0400

    Update Install_Only_Chocolatey_NonAdmin.ps1

--- Install_Only_Chocolatey_NonAdmin.ps1
+++ Install_Only_Chocolatey_NonAdmin.ps1
@@ -13,3 +13,25 @@ Set-ExecutionPolicy Bypass -Scope Process -Force;
 # All install options - offline, proxy, etc at
 # https://chocolatey.org/install
 iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
+
+dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
+
+# ahora se debe reiniciar
+
+# despues de reiniciar
+
+# Source file location
+$source = 'https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi'
+# Destination to save the file
+$destination = 'wsl_update.msi'
+#Download the file
+Invoke-WebRequest -Uri $source -OutFile $destination
+
+# ejecutar el .msi
+.\wsl_update.msi /quiet
+
+# setear a version 2 por defecto
+wsl --set-default-version 2
+
