9d7085624f58ae98c3b832589479d69edb2eef1b
Carlos Calvo <52244870+ccalvocm@users.noreply.github.com>
2022-04-28 13:51:32 -0400

    Update Desinstalar_SRM.bat

--- Desinstalar_SRM.bat
+++ Desinstalar_SRM.bat
@@ -1,3 +1,5 @@
 call choco uninstall miniconda3
 CD ..
 call rmdir /s /q %~dp0..\Demo-SRM
+call del "%USERPROFILE%\Desktop\CNR-SRM.lnk"
+mshta "about:<script>alert('Git instalado existosamente, ejecutar Etapa2.bat');close()</script>"
