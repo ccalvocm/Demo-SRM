2b96b15d72c4fc3ff5f87abe8228595ca6a227d4
ccalvocm <52244870+ccalvocm@users.noreply.github.com>
2022-04-29 15:38:12 -0400

    add: maps

--- create_HTMLmaps.py
+++ create_HTMLmaps.py
@@ -8,7 +8,7 @@ Created on Thu Oct  7 13:16:23 2021
 
 import ee
 service_account = 'srmearthenginelogin@srmlogin.iam.gserviceaccount.com'
-credentials = ee.ServiceAccountCredentials(service_account, '.\\interfaz_descarga_GEE\\srmlogin-ec74c9d43705.json')
+credentials = ee.ServiceAccountCredentials(service_account, '.\\interfaz_descarga_GEE\\srmlogin-175106b08655.json')
 ee.Initialize(credentials)
 import geemap
 import os
