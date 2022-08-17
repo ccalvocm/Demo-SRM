fb9b223123891a4ca85bdbe7ac96a1a20e8962b4
Carlos Calvo <52244870+ccalvocm@users.noreply.github.com>
2022-08-17 10:27:43 -0400

    Update pyCSRM.py

--- pyCSRM.py
+++ pyCSRM.py
@@ -326,10 +326,7 @@ def DEVELOP_SRM(folder, type_, alpha = 0.959, Tcrit = 1):
     # this code considers Snowmelt and precipitation to have separate timelags        
     tol = 1
     i = 1
-    itera=2e0
-    if '02_RMEEM' in folder:
-        itera=3e0
-    while (tol >= .2) & (i < itera):
+    while (tol >= .2) & (i < 2e0):
         
         # calcular el caudal total simulado
         loopQtotalCython.loopQtotal(0, Days, nZones, apPluv, k, X, Qtot, Y, baseflow_, tls, tlr, Qsnow, Qnewsnow, Qglacial, Qrain)
